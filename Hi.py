bbc98962bb7fc6f3c7ab314969b5c12df677d1ce
unknown <yashsh189@gmail.com>
2019-10-25 02:36:27 -0500

    commented

--- Hi.py
+++ Hi.py
@@ -1,5 +1,5 @@
 import numpy as np
 
-a = [10,20,30,40,50,60]
-name = 'Rohit'
-print('Average marks of {} are {}'.format(name,(np.sum(a))/len(a)))
+# a = [10,20,30,40,50,60]
+# name = 'Rohit'
+# print('Average marks of {} are {}'.format(name,(np.sum(a))/len(a)))
